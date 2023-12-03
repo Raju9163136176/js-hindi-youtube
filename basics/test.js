b64ace3ddb84f0add1ae9b051939123e2365c392
Raju Mondal <mailmeraju92@gmail.com>
2023-12-03 16:31:14 +0000

    the basics

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,35 @@
-console.log("Raji")
+//console.log("Raji")
+
+//operations
+// console.log(2**65)
+// console.log("3"+"2")
+// const rji =function(){
+//     console.log("log")
+// }
+// console.log( typeof rji)
+// let jio ="a new age telecom organixation"
+// console.log( jio)
+// const name="Rajju"
+// const repoCount="4"
+// console.log(`Hello My name is ${name} and my Repository Count is ${repoCount}`)
+// const newstringName = new String('Ravvvvvvvvvvvvju')
+// console.log(newstringName.length)
+// console.log(newstringName[10])
+// // console.log(newstringName.substring(0,5))
+// console.log(Math.ceil(65.36))
+// let theDate = new Date();
+// console.log(theDate.toLocaleString())
+// console.log(theDate.getFullYear())
+
+// const myArr = [0,1012,2566,1235,2,2,3,5,58,"jjjj",["a",["ss",5,8566]],["v","ccc","cccca"]]
+// const myArr2 = [0,1012,2566,1235,2,2,3,5,58,"jjjj",["a",["ss",5,8566]],["v","ccc","cccca"]]
+
+// // console.log(myArr.push("6"))
+// // console.log(myArr.concat(myArr2))
+// console.log(Array.from("Raju"))
+
+// Singleton is created with the help of constructors
+// in order to use "Symbol" a primitive type in an object you need to put the key in [] and the values in th
+//
+
+
